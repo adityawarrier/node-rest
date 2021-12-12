36c0f694fcb08b3ee5f97f0f2ae00d447d211e2e
Aditya Warrier <aditya.warrier.unni2@gmail.com>
2021-12-12 20:54:22 +0530

    Added a user model

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+interface IUser {
+  email: string;
+  password: string;
+  name: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = model<IUser>("User", userSchema);
